551826200e00723e36d9da5cf92e52d2efca7ed5
Vishal Ola <vishalola@Vishals-MacBook-Air.local>
2022-12-26 20:22:43 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import App from './App'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-  BrowserRouter
-} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-          <Route index element={<App/>}/>
-
-    </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>
 );
 
